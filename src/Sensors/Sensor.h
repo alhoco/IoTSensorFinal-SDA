46b1c8ac14ddb41911a651a00ff110889f023e13
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-05-26 07:41:10 -0500

    Creacion del repositorio en github con lo archivos ya existentes main y sensors

---
+++ src/Sensors/Sensor.h
@@ -0,0 +1,60 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <arduino.h>
+
+
+class Sensor {
+    public: 
+        Sensor(uint8_t id, uint8_t pin);
+        uint16_t getValue();
+        uint8_t getId();
+        uint8_t getPin();
+        bool hasChanged();
+        void execute();   
+    private:
+        uint8_t pin;
+        uint16_t raw_value;
+        uint16_t value;
+        uint8_t id;
+        uint16_t read();
+        bool changed;
+        uint8_t precision = 5;
+};
+
+Sensor::Sensor(uint8_t id, uint8_t pin){
+    this->id = id;
+    this->pin = pin;
+}
+
+uint16_t Sensor::getValue(){
+    this->changed = false;
+    return this->value = value;
+}
+
+uint8_t Sensor::getId(){
+    return this->id = id;
+}
+
+bool Sensor::hasChanged(){
+    return this->changed = changed;
+}
+
+uint8_t Sensor::getPin(){
+    return this->pin = pin;
+}
+
+uint16_t Sensor::read(){
+    return uint16_t(analogRead(this->pin)); 
+}
+
+void Sensor::execute(){
+    uint16_t new_value = this->read();
+    if (abs((new_value - this->raw_value)) >= precision){
+        this->raw_value = new_value;
+        this->value = new_value;
+        this->changed = true;
+    }
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <Sensors/Sensor.h>
+#include <llp.h>
+
+
+Sensor A_Sensor = Sensor(0xA0, A0);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A_Sensor.getPin(), INPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  A_Sensor.execute();
+
+  if (A_Sensor.hasChanged()){
+    uint16_t value = A_Sensor.getValue();
+    DataPack output = DataPack();
+
+    output.addData(A_Sensor.getId(), value);
+    output.write(Serial);
+
+    // Serial.print("Valor Sensor: ");
+    // Serial.println(value);
+  }
+}
