85b0276660e38525a97f27777cddf47a073651af
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-06-01 21:38:38 -0500

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
+// Sensor reader module: 
+// Irene Abad Londoño & Alejandro Hoyos Correa
+
 #include <Arduino.h>
 #include <Sensors/Sensor.h>
 #include <llp.h>
 
-
-
 Sensor A_Sensor = Sensor(0xA0, A0);
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(A_Sensor.getPin(), INPUT);
 }
 
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
   A_Sensor.execute();
 
   if (A_Sensor.hasChanged()){
